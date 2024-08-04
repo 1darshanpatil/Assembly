e6c63eb30665cf613f4acbe55ac0a5b12e6ff17a
maxdarwin <72539638+1darshanpatil@users.noreply.github.com>
2024-08-04 03:14:40 +0000

    after-execution

--- chapter2/exit.s
+++ chapter2/exit.s
@@ -1,32 +1,22 @@
-#PURPOSE:  Simple program that exits and returns a status code back to the Linux kernel
-#          status code back to the Linux kernel
+# PURPOSE:  Simple program that exits and returns a status code to the Linux kernel.
 
+# INPUT:    None
 
-#INPUT:    none
-#
+# OUTPUT:   Returns a status code. This can be viewed by typing `echo $?` after the execution of the program.
+#           It is recommended to execute some failed code before exiting, as very often `echo $?` returns `0`
+#           due to previous successful executions.
 
-#OUTPUT:   returns a status code. This can be viewed by typing `echo $?` after the execution of the program.
-#          I would recommend that you execute some failed code before exit.s; As very often `echo $?` returns `0` 
-#          because of our previous executions.
-
-#VARIABLES:
-#         %eax holds the system call number
-#         %ebx holds the return status
+# VARIABLES:
+#           %eax holds the system call number.
+#           %ebx holds the return status.
 
 .section .data
 .section .text
 .globl _start
 _start:
-movl $1, %eax  # This is the linux kernel command
-               # number (system call) for exiting
-               # a program.
+    movl $1, %eax   # This is the Linux kernel command number (system call) for exiting a program.
 
+    movl $0, %ebx   # This is the status number we will return to the operating system.
+                    # Change this value to return different statuses to echo $?.
 
-movl $0, %ebx # This is the status number we will
-              # return to the operating system.
-              # Change this around and it will will
-              # return different things to
-              # echo $?
-
-int $0x80    # this wakes up the kernel to run
-             # the exit command
+    int $0x80       # This wakes up the kernel to run the exit command.
